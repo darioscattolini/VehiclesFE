57feefde220a400446f5f11f02bc2f3ec6172160
Darío Scattolini <dario.scattolini@gmail.com>
2020-02-12 12:21:09 +0100

    Phase 1

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,25 @@
 "use strict";
+function retrieveInput(inputId) {
+    return document.querySelector("#" + inputId).value;
+}
 function createCar(plate, brand, color) {
     var car = new Car(plate, color, brand);
     car.addWheel(new Wheel(2, "SEAT"));
-    document.body.innerText = "CAR: PLATE: " + car.plate
-        + " COLOR: " + car.color + " BRAND: " + brand
-        + " WHEELS: " + JSON.stringify(car.wheels);
+    displayCarInfo(car);
+}
+function callCreateCar() {
+    var plate = retrieveInput("plate");
+    var brand = retrieveInput("brand");
+    var color = retrieveInput("color");
+    createCar(plate, brand, color);
+}
+function displayCarInfo(car) {
+    var display = document.querySelector("#carInfo");
+    display.innerHTML = "<p>The new car was successfully created:<p>";
+    var carData = document.createElement("table");
+    carData.innerHTML += "<tr><td>Plate</td> <td>" + car.plate + "</td></tr>";
+    carData.innerHTML += "<tr><td>Brand</td> <td>" + car.brand + "</td></tr>";
+    carData.innerHTML += "<tr><td>Color</td> <td>" + car.color + "</td></tr>";
+    carData.innerHTML += "<tr><td>Wheels</td> <td><pre>" + JSON.stringify(car.wheels, undefined, 2) + "</pre></td></tr>";
+    display.append(carData);
 }
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,9 +1,27 @@
-
-function createCar(plate:string,brand:string,color:string){
-    let car=new Car(plate,color,brand);
-    car.addWheel(new Wheel(2,"SEAT"));
-    document.body.innerText="CAR: PLATE: " + car.plate 
-    + " COLOR: " +car.color + " BRAND: " + brand 
-    + " WHEELS: " + JSON.stringify(car.wheels);
+function retrieveInput(inputId: string) {
+    return (<HTMLInputElement>document.querySelector(`#${inputId}`)).value;
 }
 
+function createCar(plate:string, brand:string, color:string){
+    let car = new Car(plate, color, brand);
+    car.addWheel(new Wheel(2, "SEAT"));
+    displayCarInfo(car);
+}
+
+function callCreateCar() {
+    const plate = retrieveInput("plate");
+    const brand = retrieveInput("brand");
+    const color = retrieveInput("color");
+    createCar(plate, brand, color);
+}
+
+function displayCarInfo(car: Car) {
+    const display = <HTMLDivElement>document.querySelector("#carInfo");
+    display.innerHTML = "<p>The new car was successfully created:<p>";
+    const carData = document.createElement("table");
+    carData.innerHTML += `<tr><td>Plate</td> <td>${car.plate}</td></tr>`;
+    carData.innerHTML += `<tr><td>Brand</td> <td>${car.brand}</td></tr>`;
+    carData.innerHTML += `<tr><td>Color</td> <td>${car.color}</td></tr>`;
+    carData.innerHTML += `<tr><td>Wheels</td> <td><pre>${JSON.stringify(car.wheels, undefined, 2)}</pre></td></tr>`;
+    display.append(carData);
+}
